a271dfda581c03db2c9a4f236d26cd1295928cdb
DeveloperBhin <norbeth559@gmail.com>
2025-12-11 15:02:17 +0300

    modify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/ZubedaNurdin-portfolio/', // very important for correct routing
+    base: '/ZuuPortifolioweb/', // very important for correct routing
   server: {
     watch: {
       usePolling: true,
