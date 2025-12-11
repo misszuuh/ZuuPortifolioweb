3cf4c9edca4a645b4b2aa57c1fa2166ab68dfe02
DeveloperBhin <norbeth559@gmail.com>
2025-12-11 15:30:39 +0300

    modify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ZuuPortifolioweb/', // <-- ensures correct paths in the built files
+  base: '/', // <-- ensures correct paths in the built files
   plugins: [react()],
 })
